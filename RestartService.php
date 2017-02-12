4145bce44af5ecac8b80286948fafffeb09b9fd0
amorospo <amorospo@yahoo.it>
2017-02-12 16:10:55 +0100

    Delete RestartService.php

--- RestartService.php
+++
@@ -1,7 +0,0 @@
-<?php
-$output = shell_exec('./RestartService.sh');
-echo "<pre>$output</pre>";
-echo "<br><br><a href=\"javascript:history.go(-2)\">Torna indietro</a>";
-//$output = shell_exec('ls -lart');
-//echo "<pre>$output</pre>";
-?>
